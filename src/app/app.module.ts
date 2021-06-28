1ae482a16837a32925416865db059ee958b50087
Ali Rassai <helpdesk@koolak.ca>
2021-06-28 13:07:06 -0400

    v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
